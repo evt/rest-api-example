0b5b2450ad2769591826103fa9847b5b6ab6d21f
Eugene Toropov <eugene.toropov@gmail.com>
2020-10-25 16:17:49 +0300

    Fixed migrations PATH

--- cmd/api/env.sh
+++ cmd/api/env.sh
@@ -7,14 +7,14 @@ export GC_BUCKET=my-cool-bucket
 
 # Postgres settings
 #export PG_URL=postgres://postgres:postgres@localhost/test?sslmode=disable
-#export PG_MIGRATIONS_PATH=file://../../pgdb/migrations
+#export PG_MIGRATIONS_PATH=file://../../store/pg/migrations
 
 # MySQL settings
 export MYSQL_ADDR=127.0.0.1:3306
 export MYSQL_USER=api
 export MYSQL_PASSWORD=api
 export MYSQL_DB=apiserver
-export MYSQL_MIGRATIONS_PATH=file://../../mysqldb/migrations
+export MYSQL_MIGRATIONS_PATH=file://../../store/mysql/migrations
 
 # Logger settings
 export LOG_LEVEL=debug
